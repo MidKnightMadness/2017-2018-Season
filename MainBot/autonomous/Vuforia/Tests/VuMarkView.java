17c52625ced0f7cced274332bc313a7da9f58723
glingy <gregory.ling@students.tka.org>
2017-10-17 07:20:59 -0700

    Reformatted VisualController and added demo mainbot autonomous program.

--- MainBot/autonomous/Vuforia/Tests/VuMarkView.java
+++ MainBot/autonomous/Vuforia/Tests/VuMarkView.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.MainBot.autonomous.Vuforia;
+package org.firstinspires.ftc.teamcode.MainBot.autonomous.Vuforia.Tests;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
