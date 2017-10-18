ac2bdc83eb48e51d1bf3b7609bfb2ec06f22725b
glingy <gregory.ling@students.tka.org>
2017-10-18 09:22:15 -0700

    Moved the jewelDriveBot's controller to the MainBot controller and changed from simpleDrive to the full drive

--- MainBot/teleop/MainBot/MainBotController.java
+++ MainBot/teleop/MainBot/MainBotController.java
@@ -4,26 +4,42 @@ import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
-import org.firstinspires.ftc.teamcode.MainBot.teleop.CrossCommunicator;
+import org.firstinspires.ftc.teamcode.MainBot.teleop.DriveAssembly.DriveAssemblyController;
+import org.firstinspires.ftc.teamcode.MainBot.teleop.SimpleDriveAssembly.SimpleDriveAssemblyController;
+import org.firstinspires.ftc.teamcode.MainBot.teleop.JewelAssembly.JewelAssemblyController;
+
+/**
+ * Created by nisha.prasad on 10/17/17.
+ */
 
 public class MainBotController {
 
     private Telemetry telemetry;
+    private DriveAssemblyController driveAssemblyController;
+    private JewelAssemblyController jewelAssemblyController;
+
+    public MainBotController() {
+        jewelAssemblyController = new JewelAssemblyController();
+        driveAssemblyController = new DriveAssemblyController();
+    }
 
     public void init(Telemetry telemetry, HardwareMap hardwareMap) {
-        this.telemetry = telemetry;
-
+        jewelAssemblyController.init(telemetry, hardwareMap);
+        driveAssemblyController.init(telemetry, hardwareMap);
     }
 
     public void start() {
-
+        jewelAssemblyController.start();
+        driveAssemblyController.start();
     }
 
     public void loop(Gamepad gamepad1, Gamepad gamepad2) {
-
+        jewelAssemblyController.loop(gamepad1, gamepad2);
+        driveAssemblyController.loop(gamepad1, gamepad2);
     }
 
     public void stop() {
-
+        jewelAssemblyController.stop();
+        driveAssemblyController.stop();
     }
 }
