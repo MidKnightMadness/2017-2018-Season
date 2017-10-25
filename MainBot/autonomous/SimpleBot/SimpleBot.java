c98d04a4d35e525521aaf04dd7740f3b0b1174d8
Brendan Peercy <nbabsp@gmail.com>
2017-10-25 16:43:29 -0700

    fixed Gregory's error

--- MainBot/autonomous/SimpleBot/SimpleBot.java
+++ MainBot/autonomous/SimpleBot/SimpleBot.java
@@ -4,7 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.MainBot.autonomous.Vuforia.VisualController;
+import org.firstinspires.ftc.teamcode.MainBot.autonomous.VisualAssembly.VisualController;
 import org.firstinspires.ftc.teamcode.MainBot.teleop.CrossCommunicator;
 
 @Autonomous(name = "MainBot", group = "Main Bot")
