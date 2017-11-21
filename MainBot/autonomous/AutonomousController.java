402cc3f66d920aaf447af0420687ba333f807f7d
glingy <gregory.ling@students.tka.org>
2017-11-20 21:49:26 -0800

    Added VEX Servo Test, updated Glyph assembly, added new Autonomous controller.

---
+++ MainBot/autonomous/AutonomousController.java
@@ -0,0 +1,126 @@
+package org.firstinspires.ftc.teamcode.MainBot.autonomous;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.sun.tools.javac.util.ArrayUtils;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.MainBot.teleop.CrossCommunicator;
+
+
+public class AutonomousController {
+
+    //TODO: CHECK WIRING ON JEWEL ARM: REVERSED? WHY REVERSED SPEED AND ENCODERS??
+
+    public static int GLYPH = 0;
+    public static int JEWEL = 1;
+    public static int UP = 2;
+    public static int DOWN = 3;
+    public static int LEFT = 4;
+    public static int RIGHT = 5;
+
+    private static double DRIVE_SPEED = 0.4;
+    private static double ROTATE_SPEED = 0.3;
+
+
+
+    public Servo glyphServo;
+    public int[] pos = new int[6];
+    public DcMotor[] motors = new DcMotor[6];
+
+    private VisualController visualC = new VisualController();
+    private static VisualController.JewelColor TEAM_COLOR = VisualController.JewelColor.RED;
+
+    private void move(int motor, int encoder, double speed) {
+        motors[motor].setTargetPosition(motors[motor].getCurrentPosition() - pos[motor] + encoder);
+        motors[motor].setPower(-speed);
+    }
+
+    public void init(Telemetry telemetry, HardwareMap hardwareMap) {
+        motors[GLYPH] = hardwareMap.dcMotor.get(CrossCommunicator.Glyph.MOTOR);
+        motors[GLYPH].resetDeviceConfigurationForOpMode();
+        motors[GLYPH].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motors[GLYPH].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        pos[GLYPH] = motors[GLYPH].getCurrentPosition();
+
+        motors[JEWEL] = hardwareMap.dcMotor.get(CrossCommunicator.Jewel.MOTOR);
+        motors[JEWEL].resetDeviceConfigurationForOpMode();
+        motors[JEWEL].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motors[JEWEL].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        pos[JEWEL] = motors[JEWEL].getCurrentPosition();
+
+        motors[UP] = hardwareMap.dcMotor.get(CrossCommunicator.Drive.UP);
+        motors[UP].resetDeviceConfigurationForOpMode();
+        motors[UP].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motors[UP].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        pos[UP] = motors[UP].getCurrentPosition();
+
+        motors[DOWN] = hardwareMap.dcMotor.get(CrossCommunicator.Drive.DOWN);
+        motors[DOWN].resetDeviceConfigurationForOpMode();
+        motors[DOWN].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motors[DOWN].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        pos[DOWN] = motors[DOWN].getCurrentPosition();
+
+        motors[LEFT] = hardwareMap.dcMotor.get(CrossCommunicator.Drive.LEFT);
+        motors[LEFT].resetDeviceConfigurationForOpMode();
+        motors[LEFT].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motors[LEFT].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        pos[LEFT] = motors[LEFT].getCurrentPosition();
+
+        motors[RIGHT] = hardwareMap.dcMotor.get(CrossCommunicator.Drive.RIGHT);
+        motors[RIGHT].resetDeviceConfigurationForOpMode();
+        motors[RIGHT].setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        motors[RIGHT].setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        pos[RIGHT] = motors[RIGHT].getCurrentPosition();
+
+        glyphServo = hardwareMap.servo.get(CrossCommunicator.Glyph.SERVO);
+        glyphServo.setPosition(0.6);
+
+        visualC.init(telemetry, hardwareMap);
+    }
+
+    public void close() {
+        glyphServo.setPosition(0);
+    }
+    public void open() {
+        glyphServo.setPosition(0.5);
+    }
+    public void lift() {
+        move(GLYPH, 1000, 1);
+    }
+    public void lower() {
+        move(GLYPH, -1000, -1);
+    }
+
+    public void moveBot(int distance) {
+        double speed = DRIVE_SPEED;
+        if (Math.signum(distance) != Math.signum(speed))
+            speed *= -1;
+        move(UP, distance, speed);
+        move(LEFT, distance, speed);
+        move(DOWN, -distance, -speed);
+        move(RIGHT, -distance, -speed);
+    }
+
+    public void rotateBot(int distance, double speed) {
+        if (Math.signum(distance) != Math.signum(speed))
+            speed *= -1;
+        move(UP, distance, speed);
+        move(LEFT, distance, speed);
+        move(DOWN, distance, speed);
+        move(RIGHT, distance, speed);
+    }
+
+    public void rotateBot(int distance) {
+        rotateBot(distance, ROTATE_SPEED * Math.signum(distance));
+    }
+
+    public void lowerJArm() {
+        move(JEWEL, 625, 0.3);
+    }
+
+    public void raiseJArm() {
+        move(JEWEL, -625, -0.3);
+    }
+}
--- MainBot/teleop/GlyphAssembly/GlyphAssemblyController.java
+++ MainBot/teleop/GlyphAssembly/GlyphAssemblyController.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Gamepad;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.teamcode.MainBot.teleop.CrossCommunicator;
@@ -16,9 +17,14 @@ public class GlyphAssemblyController {
     private boolean open;
     private int pos;
     private double servoPos;
+    private int elevatorTarget;
+    private int futureTarget;
+    private int targetTime;
+    private ElapsedTime time;
 
     public void init(Telemetry telemetry, HardwareMap hardwareMap) {
         this.telemetry = telemetry;
+        time = new ElapsedTime();
         motor = hardwareMap.dcMotor.get(CrossCommunicator.Glyph.MOTOR);
         motor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         motor.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
@@ -27,20 +33,34 @@ public class GlyphAssemblyController {
         servo.setPosition(0.5);
         open = true;
         pos = motor.getCurrentPosition();
-        servoPos = 1;
+        servoPos = 0.5;
+        elevatorTarget = -1;
+        futureTarget = -1;
+        targetTime = 0;
     }
 
     public void start() {
 
     }
 
+    private int elevPos() {
+        return (motor.getCurrentPosition() - pos);
+    }
+
     public void loop(Gamepad gamepad1, Gamepad gamepad2) {
 
-        telemetry.addData("Elevator", pos - motor.getCurrentPosition());
-        if (gamepad1.right_bumper) {
-            motor.setPower((motor.getCurrentPosition() - pos < 10300 || gamepad1.x) ? 1 : 0);
-        } else if (gamepad1.left_bumper) {
-            motor.setPower((motor.getCurrentPosition() - pos > 0 || gamepad1.x) ? -1 : 0);
+        if (gamepad1.right_bumper || gamepad1.left_bumper/* || gamepad2.left_stick_y > 0.1 || gamepad2.left_stick_y < 0.1*/) {
+            elevatorTarget = -1;
+        }
+        telemetry.addData("Elevator", elevPos());
+        if (gamepad1.right_bumper || ((elevatorTarget - elevPos()) > 100 && elevatorTarget != -1)) {
+            motor.setPower((elevPos() < 6600 || gamepad1.x) ? 1 : 0);
+        } else if (gamepad1.left_bumper ||((elevatorTarget - elevPos()) < -100 && elevatorTarget != -1)) {
+            motor.setPower((elevPos() > 0 || gamepad1.x) ? -1 : 0);
+        } else if (gamepad2.left_stick_y > 0.1) {
+            motor.setPower((elevPos() < 6600) ? gamepad2.left_stick_y : 0);
+        } else if (gamepad2.left_stick_y < 0.1) {
+            motor.setPower((elevPos() > 0) ? gamepad2.left_stick_y : 0);
         } else {
             motor.setPower(0);
         }
@@ -49,36 +69,55 @@ public class GlyphAssemblyController {
             pos = motor.getCurrentPosition();
         }
 
-        /*if (gamepad2.left_stick_y > 0.1) {
-            motor.setPower((motor.getCurrentPosition() - pos < 10300) ? gamepad2.left_stick_y : 0);
-        } else if (gamepad2.left_stick_y < 0.1) {
-            motor.setPower((motor.getCurrentPosition() - pos > 0) ? gamepad2.left_stick_y : 0);
-        }*/
+        if (time.milliseconds() > targetTime && futureTarget != -1) {
+            elevatorTarget = futureTarget;
+            futureTarget = -1;
+        }
 
-
-        if (gamepad1.dpad_left || gamepad2.left_trigger > 0) {
+        if (gamepad1.dpad_left || gamepad1.right_trigger > 0) {
             if (servoPos < 0.1) {
                 servoPos = 0;
             } else {
                 servoPos -= 0.1;
             }
-        } else if (gamepad1.dpad_right || gamepad2.right_trigger > 0){
-            if (servoPos > 0.5) {
-                servoPos = 0.6;
+        } else if (gamepad1.dpad_right || gamepad1.left_trigger > 0){
+            if (servoPos > 0.45) {
+                servoPos = 0.55;
             } else {
                 servoPos += 0.1;
             }
         }
 
+        if (gamepad1.dpad_up) {
+            grab(2);
+        } else if (gamepad1.dpad_down) {
+            release();
+        }
+
 
 
         telemetry.addData("Servo", servoPos);
+        telemetry.addData("Buttons", gamepad1.right_bumper || gamepad1.left_bumper/* || gamepad2.left_stick_y > 0.1 || gamepad2.left_stick_y < 0.1*/);
         telemetry.addData("Elev", motor.getCurrentPosition());
+        telemetry.addData("Elevator Target", elevatorTarget);
+        telemetry.addData("Future Target", futureTarget);
+        telemetry.addData("Time Remaining", time.milliseconds() - targetTime);
         servo.setPosition(servoPos);
 
 
         telemetry.addLine("Servo Position: " + servo.getPosition());
+    }
 
+
+    public void grab(int level) {
+        servoPos = 0;
+        targetTime = (int)time.milliseconds() + 1000;
+        futureTarget = level * 1250;
+    }
+
+    public void release() {
+        elevatorTarget = elevPos() - 200;
+        servoPos = 0.5;
     }
 
     public void stop() {
