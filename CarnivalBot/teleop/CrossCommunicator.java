1bf7501384fef43b4927f10971658145fe1d304d
glingy <gregory.ling@students.tka.org>
2018-02-27 18:28:43 -0800

    remove CarnivalBot

--- CarnivalBot/teleop/CrossCommunicator.java
+++
@@ -1,9 +0,0 @@
-package org.firstinspires.ftc.teamcode.CarnivalBot.teleop;
-
-public class CrossCommunicator {
-
-    public static class Drive {
-        public static final String MOTOR_LEFT = "left";
-        public static final String MOTOR_RIGHT = "right";
-    }
-}
